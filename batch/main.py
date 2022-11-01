27b735882dfdedbaede0d4c5d8587bdac5785f08
yyyoichi <origamiichiro@gmail.com>
2022-11-01 16:22:03 +0900

    add env file

--- batch/main.py
+++ batch/main.py
@@ -1,5 +1,12 @@
+import setting.setting as setting
+
+
 def main():
     print("Hello world")
+    if setting.ENV == "pro":
+        print("ENV: 本番環境")
+    else:
+        print("ENV: 開発環境")
 
 
 if __name__ == "__main__":
