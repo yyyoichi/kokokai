6b0c20b8fb02c3ba0042a4fe42b7e43fc1329f93
yyyoichi <origamiichiro@gmail.com>
2022-11-03 08:52:19 +0900

    add validation

--- batch/main.py
+++ batch/main.py
@@ -1,5 +1,28 @@
 from src.main import daySpeech
-from src.word import getMecab
+from src.word import getMecab, Word
+
+
+class Validation:
+    stop_words = [""]
+
+    def __init__(self, word: Word) -> None:
+        self.w = word
+
+    def is_stop_word(self):
+        return self.w.surface() in self.stop_words
+
+    def is_noun(self):
+        return self.w.part_of_speech() == "名詞"
+
+    def is_int(self):
+        return self.w.part_of_speech_details_1() == "数"
+
+    def is_asterisk(self):
+        return self.w.prototype() == "*"
+
+
+with open('stop_words.txt', 'r', encoding="utf-8") as f:
+    Validation.stop_words = [w.strip() for w in f.readlines()]
 
 
 def main():
@@ -12,7 +35,7 @@ def main():
         w = next(p, None)
         while (w):
             w.surface()
-            print(w.surface() + " - " + w.part_of_speech())
+            # print(w.surface() + " - " + w.part_of_speech())
             w = next(p, None)
         s = next(it, None)
 
