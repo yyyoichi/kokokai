6a99153ad8082f7892297fe880991df987a111b4
yyyoichi <origamiichiro@gmail.com>
2022-11-02 23:05:05 +0900

    get sentens step by step

--- batch/main.py
+++ batch/main.py
@@ -1,19 +1,17 @@
+from src.main import daySpeech
 import src.setting as setting
 import src.db as db
-import src.main as mecab
 
 
 def main():
-    print("Hello world")
-    if setting.ENV == "pro":
-        print("ENV: 本番環境")
-    else:
-        print("ENV: 開発環境")
-    d = db.DB()
-    d.test()
-    d.close()
-    m = mecab.getMecab()
-    m.parse("鬼滅の刃")
+    date = "2022-03-01"
+    it = daySpeech(date)
+    last = "1"
+    s = next(it, None)
+    while (s):
+        last = s
+        s = next(it, None)
+    print(last)
 
 
 if __name__ == "__main__":
