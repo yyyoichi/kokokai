973a62040b7798cb4f0468a59810fadf9d66ea5c
yyyoichi <origamiichiro@gmail.com>
2022-11-03 09:15:26 +0900

    get target words

--- batch/main.py
+++ batch/main.py
@@ -1,24 +1,24 @@
 from src.sentence import daySpeech
-from mcb import getMecab, Morpheme
+from src.mcb import getMecab, Morpheme
 
 
 class Validation:
     stop_words = [""]
 
-    def __init__(self, word: Morpheme) -> None:
-        self.w = word
+    def __init__(self, morpheme: Morpheme) -> None:
+        self.m = morpheme
 
     def is_stop_word(self):
-        return self.w.surface() in self.stop_words
+        return self.m.surface() in self.stop_words
 
     def is_noun(self):
-        return self.w.part_of_speech() == "名詞"
+        return self.m.part_of_speech() == "名詞"
 
     def is_int(self):
-        return self.w.part_of_speech_details_1() == "数"
+        return self.m.part_of_speech_details_1() == "数"
 
     def is_asterisk(self):
-        return self.w.prototype() == "*"
+        return self.m.prototype() == "*"
 
 
 with open('stop_words.txt', 'r', encoding="utf-8") as f:
@@ -26,18 +26,31 @@ with open('stop_words.txt', 'r', encoding="utf-8") as f:
 
 
 def main():
+    """
+    その日の共起リストをDBに格納する
+    """
     m = getMecab()
     date = "2022-10-27"
     speech = daySpeech(date)
     sentence = next(speech, None)
+    # 名詞の単語リスト
+    noun_list = []
+
+    def add_noun_list(mph: Morpheme):
+        v = Validation(mph)
+        if v.is_noun() and not v.is_asterisk() and not v.is_int() and not v.is_stop_word():
+            noun_list.append(mph.prototype())
+
     while (sentence):
         p = m.parse(sentence)
         morpheme = next(p, None)
         while (morpheme):
-            morpheme.surface()
-            # print(w.surface() + " - " + w.part_of_speech())
+            add_noun_list(morpheme)
             morpheme = next(p, None)
         sentence = next(speech, None)
+
+    for w in noun_list:
+        print(w)
 
 
 if __name__ == "__main__":
