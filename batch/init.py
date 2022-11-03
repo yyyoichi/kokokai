75d3b7be723b0e0fe5dac3709823aba079e5f971
yyyoichi <origamiichiro@gmail.com>
2022-11-03 13:31:31 +0900

    fix typo

--- batch/init.py
+++ batch/init.py
@@ -3,9 +3,13 @@ from src.db import get_connection
 
 def init():
     conn = get_connection()
-    cursor = conn.corsor()
+    cursor = conn.cursor()
     with open("init.sql", "r", encoding="utf-8") as f:
         sql = f.read()
         cursor.execute(sql)
         conn.commit()
     conn.close()
+
+
+if __name__ == "__main__":
+    init()
