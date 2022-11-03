9c6da094811899b54982ea840f26093fb9c88bff
yyyoichi <origamiichiro@gmail.com>
2022-11-03 13:12:08 +0900

    fix serialize

--- batch/init.sql
+++ batch/init.sql
@@ -1,13 +1,13 @@
-create table kyoki_day (kyoki_day_id integer primary key, date date);
+create table kyoki_day (kyoki_day_id serial primary key, date date);
 create table kyoki(
-    kyoki_id integer primary key,
+    kyoki_id serial primary key,
     kyoki_day_id integer,
     freq integer
 );
 create table kyoki_item (
-    kyoki_item_id integer primary key,
+    kyoki_item_id serial primary key,
     kyoki_id integer,
     kyoki_day integer,
     word_id integer
 );
-create table word (word_id integer, word varchar(100));
+create table word (word_id serial, word varchar(100));
