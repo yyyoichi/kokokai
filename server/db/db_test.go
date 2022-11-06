35a1402e2d916b0d08651546cf16cc886d89a53b
yyyoichi <origamiichiro@gmail.com>
2022-11-06 10:18:33 +0900

    add convert test

--- server/db/db_test.go
+++ server/db/db_test.go
@@ -57,3 +57,66 @@ func TestQueryDatabase(t *testing.T) {
 	}
 	defer db.Close()
 }
+
+func TestNullXXConvert(t *testing.T) {
+	db, err := GetDatabase()
+	if err != nil {
+		t.Errorf("cannot connect db.")
+	}
+	rows, err := db.Query(`select * from kyokiday where pk > 0 limit 1`)
+	if err != nil {
+		panic(err)
+	}
+	if rows.Next() {
+		var pk sql.NullInt64
+		var date sql.NullTime
+		var createAt sql.NullTime
+		err := rows.Scan(&pk, &date, &createAt)
+		if err != nil {
+			t.Error("error scan")
+		}
+		testingInteger(t, pk)
+		testingTime(t, date)
+		testingTime(t, createAt)
+	}
+	rows, err = db.Query(`select * from word where word is not null limit 1`)
+	if err != nil {
+		panic(err)
+	}
+	if rows.Next() {
+		var code sql.NullInt64
+		var word sql.NullString
+		err := rows.Scan(&code, &word)
+		if err != nil {
+			t.Error("error scan")
+		}
+		testingInteger(t, code)
+		testingString(t, word)
+	}
+	defer db.Close()
+}
+
+func testingString(t *testing.T, v interface{}) {
+	nv, ok := v.(sql.NullString)
+	if !ok {
+		t.Errorf("exp not sql.NullString. got=%T", v)
+	}
+	s := n2s(nv)
+	t.Log(s)
+}
+func testingInteger(t *testing.T, v interface{}) {
+	nv, ok := v.(sql.NullInt64)
+	if !ok {
+		t.Errorf("exp not sql.NullInt64. got=%T", v)
+	}
+	s := n2i(nv)
+	t.Log(s)
+}
+func testingTime(t *testing.T, v interface{}) {
+	nv, ok := v.(sql.NullTime)
+	if !ok {
+		t.Errorf("exp not sql.NullTime. got=%T", v)
+	}
+	s := n2t(nv)
+	t.Log(s)
+}
