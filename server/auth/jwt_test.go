b012749cb773d966ff712305ce3b1936169fe125
yyyoichi <origamiichiro@gmail.com>
2022-11-23 22:38:10 +0900

    add: invalid token test

--- server/auth/jwt_test.go
+++ server/auth/jwt_test.go
@@ -17,3 +17,20 @@ func TestJWTToken(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestInvalidToken(t *testing.T) {
+	secret := "secret!!00"
+	j := NewJwtToken(secret)
+	tokenString, err := j.Generate(&User{Name: "yyyoichi", Email: "yyyoichi@example.com"})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(*tokenString)
+	j.secret = "secret!!11"
+	err = j.ParseToken(*tokenString)
+	if err == nil {
+		t.Error("??")
+	} else {
+		t.Log(err)
+	}
+}
