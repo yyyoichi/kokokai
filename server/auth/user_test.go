8b54c79888b72dc8a4a0bccb588da332ee017eec
yyyoichi <origamiichiro@gmail.com>
2022-11-23 22:25:37 +0900

    add: user_test

--- server/auth/user_test.go
+++ server/auth/user_test.go
@@ -1,7 +1,65 @@
 package auth
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
 
-func TestingCreate(t *testing.T) {
+	"github.com/joho/godotenv"
+)
 
+func testLoadEnv() {
+	currentDir, _ := os.Getwd()
+	envPath := strings.ReplaceAll(filepath.Join(currentDir, "../.env"), "\\", "/")
+	err := godotenv.Load(envPath)
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	testLoadEnv()
+	u := &User{Email: "yyyoichi@example.com", Pass: "pa55w0rd"}
+	err := u.Create()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if u.Pk == 0 {
+		t.Error("Pk is 0.")
+	}
+	if u.Id == "" {
+		t.Error("Id in ''")
+	}
+	t.Logf("Pk: %d, Email: %s, Id: %s, Pass: %s", u.Pk, u.Email, u.Id, u.Pass)
+	t.Log(u.CreateAt)
+}
+func TestGetUser(t *testing.T) {
+	testLoadEnv()
+	u := &User{Email: "yyyoichi@example.com", Pass: "pa55w0rd"}
+	err := u.Get()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log(u)
+}
+func TestDeleteUser(t *testing.T) {
+	testLoadEnv()
+	u := &User{Email: "yyyoichi@example.com", Pass: "pa55w0rd"}
+	err := u.Get()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	t.Log(u)
+	err = u.Delete()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	err = u.Get()
+	if err != nil {
+		t.Logf(err.Error())
+	} else {
+		t.Errorf("exits")
+	}
 }
