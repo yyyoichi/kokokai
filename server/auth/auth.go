2fe99f7fbcd8693c1e447dbade3d1fd6f7944041
yyyoichi <origamiichiro@gmail.com>
2022-11-29 20:03:13 +0900

    add: authrizationchecker

---
+++ server/auth/auth.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"errors"
+	"os"
+)
+
+type Auth struct {
+	UserId string
+}
+
+func New() *Auth {
+	return &Auth{}
+}
+
+func (at *Auth) CheckAuth(authorizationHeader string) error {
+	// authorizationHeader := r.Header.Get("Authorization")
+	if authorizationHeader == "" {
+		return errors.New("Unauthorized")
+	}
+	secret := os.Getenv("SECRET")
+	j := NewJwtToken(secret)
+	userId, err := j.ParseToken(authorizationHeader[7:])
+	if err != nil {
+		return errors.New("invalid_request")
+	}
+	at.UserId = *userId
+	return nil
+}
