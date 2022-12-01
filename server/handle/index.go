694b586e1f7bd9dabe6bd3880d1e07fc781e05b7
yyyoichi <origamiichiro@gmail.com>
2022-12-01 21:22:26 +0900

    add: new validation

--- server/handle/index.go
+++ server/handle/index.go
@@ -3,6 +3,9 @@ package handle
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type Response struct {
@@ -20,4 +23,10 @@ func (res *Response) resError(w *http.ResponseWriter) {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Response{Status: "ok"})
+}
+
+func customAlphanumary(fl validator.FieldLevel) bool {
+	s := fl.Field().String()
+	rg := regexp.MustCompile(`([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])`)
+	return rg.MatchString(s)
 }
