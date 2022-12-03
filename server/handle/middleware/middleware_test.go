33599d1c48561dd7140ddd474a72885c9b1965f5
yyyoichi <origamiichiro@gmail.com>
2022-12-03 13:27:12 +0900

    add: auth middleware test

---
+++ server/handle/middleware/middleware_test.go
@@ -0,0 +1,122 @@
+package middleware
+
+import (
+	"encoding/json"
+	"kokokai/server/auth"
+	"kokokai/server/db/user"
+	"kokokai/server/handle"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+type authTest struct {
+	req *http.Request
+	got *httptest.ResponseRecorder
+}
+
+func (at *authTest) addAuthHeader(token string) {
+	at.req.Header = map[string][]string{
+		"Authorization": {"Bearer " + token},
+	}
+}
+
+func NewAuthTest(urlUserId string) *authTest {
+	at := &authTest{}
+	at.req = httptest.NewRequest(http.MethodPost, "/users/"+urlUserId, nil)
+	at.got = httptest.NewRecorder()
+	return at
+}
+
+func useRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.Use(MiddlewareAuth)
+	h := func(w http.ResponseWriter, r *http.Request) {
+		res := handle.Response{Status: "ok"}
+		resJson, err := json.Marshal(res)
+		if err != nil {
+			res := handle.Response{Status: err.Error()}
+			res.Error(&w)
+			return
+		}
+		w.Write(resJson)
+	}
+	r.HandleFunc("/users/{userId}", h)
+	r.HandleFunc("/users/", h)
+	http.Handle("/", r)
+	return r
+}
+func TestAuthMiddleware(t *testing.T) {
+	loadEnv()
+	router := useRouter()
+	u := user.User{Id: "yyyoichi"}
+	s := os.Getenv("SECRET")
+	auth := auth.NewJwtToken(s)
+	next := NewAuthTest(u.Id)
+
+	token, err := auth.Generate(u.Id, u.Name)
+	if err != nil {
+		t.Error(err)
+	}
+	next.addAuthHeader(*token)
+
+	router.ServeHTTP(next.got, next.req)
+	var res handle.Response
+	if err := json.NewDecoder(next.got.Body).Decode(&res); err != nil {
+		t.Error(err)
+	}
+	if res.Status != "ok" {
+		t.Errorf("expected ok. but got=%s", res.Status)
+	}
+}
+
+func TestMiddlewareAuth(t *testing.T) {
+	loadEnv()
+	id := "yyyoichi"
+	s := os.Getenv("SECRET")
+	test := []struct {
+		pathId         string
+		tokenId        string
+		secret         string
+		hasHead        bool
+		expectedStatus string
+	}{
+		{id, id, s, true, "ok"},           // 正常系
+		{"", id, s, true, "ok"},           // 正常系パラメータなし
+		{id, id, s, false, "ログインしてください。"}, //認証ヘッダなし
+		{id, id, "hoge", true, "認証に失敗しました。ログインし直してください。"}, //不正jwt.署名違い
+		{"other", id, s, true, "不正な操作です。ログインし直してください。"},   //別User操作
+	}
+	router := useRouter()
+	for _, tt := range test {
+		next := NewAuthTest(tt.pathId)
+		if tt.hasHead {
+			a := auth.NewJwtToken(tt.secret)
+			token, err := a.Generate(tt.tokenId, "")
+			if err != nil {
+				t.Error(err)
+			}
+			next.addAuthHeader(*token)
+		}
+
+		router.ServeHTTP(next.got, next.req)
+		var res handle.Response
+		if err := json.NewDecoder(next.got.Body).Decode(&res); err != nil {
+			t.Error(err)
+		}
+		if res.Status != tt.expectedStatus {
+			t.Errorf("expected status '%s'. but got=%s", tt.expectedStatus, res.Status)
+		}
+	}
+}
