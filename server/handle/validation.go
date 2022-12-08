2496644176c2c290a6d2b4812c81005dec4d8efc
yyyoichi <origamiichiro@gmail.com>
2022-12-08 18:56:33 +0900

    ref: sep signup validation

--- server/handle/validation.go
+++ server/handle/validation.go
@@ -7,6 +7,44 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+func signupValid(su *SignUp) error {
+	validate := validator.New()
+	validate.RegisterValidation("alphanumary", customAlphanumary)
+	if err := validate.Struct(su); err != nil {
+		var out bytes.Buffer
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			for _, fe := range ve {
+				switch fe.Field() {
+				case "Id":
+					if fe.Tag() == "len" {
+						out.WriteString("idは20字で入力してください。")
+					} else {
+						out.WriteString("id を入力してください。")
+					}
+				case "Pass1":
+					if fe.Tag() == "alphanumary" {
+						out.WriteString("パスワードは英数字である必要があります。")
+					} else if fe.Tag() == "min" || fe.Tag() == "max" {
+						out.WriteString("パスワードは8~24字である必要があります。")
+					} else {
+						out.WriteString("パスワードを入力してください。")
+					}
+				case "Pass2":
+					if fe.Tag() == "eqfield" {
+						out.WriteString("パスワードが一致しません。")
+					} else {
+						out.WriteString("確認用のパスワードを入力してください。")
+					}
+				}
+			}
+		}
+		return errors.New(out.String())
+	} else {
+		return nil
+	}
+}
+
 func userPatchValid(up *UserPatch) error {
 	validate := validator.New()
 	if err := validate.Struct(up); err != nil {
