b2304673e5a58049655d409c6a99eb35b0569ec8
yyyoichi <origamiichiro@gmail.com>
2022-12-09 08:51:40 +0900

    fix: typo

--- server/handle/cookie/useCookie.go
+++ server/handle/cookie/useCookie.go
@@ -34,7 +34,7 @@ func NewUserCookie(jwtToken string) *http.Cookie {
 func FromUserCookie(r *http.Request) (*http.Cookie, error) {
 	cookie, err := r.Cookie("token")
 	if err != nil {
-		return nil, errors.New("no user cookkie: " + err.Error())
+		return nil, errors.New("no user cookie: " + err.Error())
 	}
 	return cookie, nil
 }
