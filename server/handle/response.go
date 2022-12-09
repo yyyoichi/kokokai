5f7b5ae85625a30ec9801291d7b705dd66b0df99
yyyoichi <origamiichiro@gmail.com>
2022-12-09 10:46:33 +0900

    ref: res Wrapper

---
+++ server/handle/response.go
@@ -0,0 +1,53 @@
+package handle
+
+import (
+	"encoding/json"
+	cke "kokokai/server/handle/cookie"
+	"net/http"
+
+	"github.com/gorilla/csrf"
+)
+
+func NewErrorResponse(status string, w *http.ResponseWriter) {
+	res := &Response{Status: status}
+	json, err := json.Marshal(res)
+	if err != nil {
+		http.Error(*w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Error(*w, string(json), http.StatusBadRequest)
+}
+
+type Response struct {
+	Status string `json:"status"`
+}
+
+type AuthResponse struct {
+	Status string `json:"status"`
+	w      http.ResponseWriter
+	r      *http.Request
+}
+
+func (ar *AuthResponse) setJWTCookie(token string) {
+	c, err := cke.UpdateUserCookie(ar.r, token)
+	if err != nil {
+		NewErrorResponse(err.Error(), &ar.w)
+		return
+	}
+	// jwtをcookieに保存
+	http.SetCookie(ar.w, c)
+}
+
+func (ar AuthResponse) setCSRFToken() {
+	ar.w.Header().Set("X-CSRF-Token", csrf.Token(ar.r))
+}
+
+func (ar AuthResponse) writeOk() {
+	res := Response{Status: "ok"}
+	resJson, err := json.Marshal(res)
+	if err != nil {
+		NewErrorResponse(err.Error(), &ar.w)
+		return
+	}
+	ar.w.Write(resJson)
+}
